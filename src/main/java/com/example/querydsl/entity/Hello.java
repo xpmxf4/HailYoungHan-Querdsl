afeb25cf7c01a37f77729cd9ef602511a97631b4
dev-chpark <xpmxf5@gmail.com>
2023-09-13 16:05:57 +0900

    first commit

---
+++ src/main/java/com/example/querydsl/entity/Hello.java
@@ -0,0 +1,16 @@
+package com.example.querydsl.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
