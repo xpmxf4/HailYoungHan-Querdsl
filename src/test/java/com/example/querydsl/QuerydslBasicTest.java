51779df2da7f19c54d46dd42b932950984e2dd36
dev-chpark <xpmxf5@gmail.com>
2023-09-19 10:18:41 +0900

    프로젝션과 결과 반환 - 기본

--- src/test/java/com/example/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -528,4 +528,31 @@ public class QuerydslBasicTest {
             System.out.println("s = " + s);
         }
     }
+    
+    @Test
+    public void simpleProjection() {
+        List<String> fetch = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for (String name : fetch) {
+            System.out.println("name = " + name);
+        }
+    }
+
+    @Test
+    public void tupleProjection() {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
 }
