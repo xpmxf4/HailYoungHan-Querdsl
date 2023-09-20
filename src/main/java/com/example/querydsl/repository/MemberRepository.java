a2ec54806025305393d7aeee97f15335e0c48a66
dev-chpark <xpmxf5@gmail.com>
2023-09-20 16:36:07 +0900

    스프링 데이터 JPA Repository 로 변경

---
+++ src/main/java/com/example/querydsl/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.querydsl.repository;
+
+import com.example.querydsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
+}
